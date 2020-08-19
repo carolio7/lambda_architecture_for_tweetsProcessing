bf7e874023325fc9c65512afc9ccfc4fa20b650b
carolio7 <carolio7@yahoo.fr>
2020-08-20 00:48:43 +0200

    Ajout du traitement en batch

--- codes/batchLayer_sparkHdfs/tweet_batchProcessing/src/main/scala/App.scala
+++ codes/batchLayer_sparkHdfs/tweet_batchProcessing/src/main/scala/App.scala
@@ -1,3 +1,38 @@
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+
+/**
+ * @author carolio7
+ */
 object App {
 
+  // je crée mon spark session
+  val spark: SparkSession = SparkSession
+    .builder()
+    .master("local[*]")
+    .appName("twitter-batchProcess")
+    .getOrCreate()
+
+  def main(args: Array[String]): Unit = {
+    // Je définis l'addresse de HDFS
+    val addr_hdfs = "hdfs://localhost:8000"
+
+    // Lecture du flux kafka
+    val df = spark.read
+      .format("avro")
+      .load("hdfs://localhost:8000/data/twitter/full/date=2020-08-19/heure=13")
+
+    // Mettre chaque élement de  la liste en Rows
+    val exploded_df = df.selectExpr("explode(entities.hashtags)")
+    exploded_df.persist()
+
+    // Grouper, compter et ordonner les hashtags selon leur nombre
+    val htag_df = exploded_df.select("col.text")
+      .withColumnRenamed("text", "hashtags")
+      .groupBy("hashtags").count()
+      .orderBy(col("count").desc)
+      .limit(10)
+
+    htag_df.show(true)
+  }
 }
